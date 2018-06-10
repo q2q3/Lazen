972dd188c4944b082968eaa6f087a02699b756d1
Jomtek <jomtek360@yahoo.fr>
2018-06-10 02:08:35 +0200

    Update CharacterVerification.vb

--- Lazen/CharacterVerification.vb
+++ Lazen/CharacterVerification.vb
@@ -1,7 +1,5 @@
 ﻿Public Class CharacterVerification
     Public Shared Function Verify(input As String) As String
-        'cette fonction vérifie si l'input contient uniquement les caractères 
-        'du charmap précisé
         Dim blacklisted As String = "$&#, .µ""{(></\)}=:"
 
         For Each i As String In blacklisted
